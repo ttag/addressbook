1d4cbcde55f961886e6fa643f9a439291390b99a
Your Name <x.eshan@hotmail.com>
2012-01-15 18:13:00 +0500

    rails g scaffold Entry name:string address:text phone:string email:string

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AddressBook::Application.config.secret_token = '9b50b6259940126c70447d8a3a503a245368b67915c8d0c81e63a7c17db7f2e02c74542b20bca807ed312577036041e7af9d6b2c5b7a9619e4593560382766c7'
